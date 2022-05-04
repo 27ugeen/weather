3646d86857ac3f5d29daa31d93e5591d96d16c70
27ugeen <27ugeen@gmail.com>
2022-05-04 13:14:33 +0300

    added settings screen in UIView

--- Weather/Settings/SettingsViewController.swift
+++ Weather/Settings/SettingsViewController.swift
@@ -8,25 +8,112 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-    //MARK: - props
-    
-    private let settingsViewModel: SettingsViewModel
-    private let settingsCellID = SettingsTableViewCell.cellId
-    private let settingsHeaderID = SettingsHeaderView.cellId
-    private let settingsFooterID = SettingsFooterView.cellId
     //MARK: - subviews
     
-    private let tableView = UITableView(frame: .zero, style: .grouped)
+    private let windowView: UIView = {
+        let windowView = UIView()
+        windowView.translatesAutoresizingMaskIntoConstraints = false
+        windowView.backgroundColor = UIColor(rgb: 0xE9EEFA)
+        windowView.layer.cornerRadius = 8
+        return windowView
+    }()
     
-    //MARK: - init
-    init(settingsViewModel: SettingsViewModel) {
-        self.settingsViewModel = settingsViewModel
-        super.init(nibName: nil, bundle: nil)
-    }
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Настройки"
+        label.textColor = UIColor(rgb: 0x272722)
+        label.font = UIFont.setAppMainFont(18)
+        label.numberOfLines = 0
+        return label
+    }()
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    private let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Температура"
+        label.textColor = UIColor(rgb: 0x898383)
+        label.font = UIFont.setAppMainFont(16)
+        return label
+    }()
+    
+    private let windSpeedLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Скорость ветра"
+        label.textColor = UIColor(rgb: 0x898383)
+        label.font = UIFont.setAppMainFont(16)
+        return label
+    }()
+    
+    private let dateFormateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Формат времени"
+        label.textColor = UIColor(rgb: 0x898383)
+        label.font = UIFont.setAppMainFont(16)
+        return label
+    }()
+    
+    private let notificationsLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Уведомления"
+        label.textColor = UIColor(rgb: 0x898383)
+        label.font = UIFont.setAppMainFont(16)
+        return label
+    }()
+    
+    private let temperatureToggle: UISwitch = {
+        let toggle = UISwitch()
+        toggle.translatesAutoresizingMaskIntoConstraints = false
+//        toggle.isOn = UserDefaults.standard.bool(forKey: "toggle")
+        
+        toggle.onTintColor = UIColor(rgb: 0x1F4DC5)
+        return toggle
+    }()
+    
+    private let windSpeedToggle: UISwitch = {
+        let toggle = UISwitch()
+        toggle.translatesAutoresizingMaskIntoConstraints = false
+//        toggle.isOn = UserDefaults.standard.bool(forKey: "toggle")
+        
+        toggle.onTintColor = UIColor(rgb: 0x1F4DC5)
+        return toggle
+    }()
+    
+    private let dateFormateToggle: UISwitch = {
+        let toggle = UISwitch()
+        toggle.translatesAutoresizingMaskIntoConstraints = false
+//        toggle.isOn = UserDefaults.standard.bool(forKey: "toggle")
+        
+        toggle.onTintColor = UIColor(rgb: 0x1F4DC5)
+        return toggle
+    }()
+    
+    private let notificationsToggle: UISwitch = {
+        let toggle = UISwitch()
+        toggle.translatesAutoresizingMaskIntoConstraints = false
+//        toggle.isOn = UserDefaults.standard.bool(forKey: "toggle")
+        
+        toggle.onTintColor = UIColor(rgb: 0x1F4DC5)
+        return toggle
+    }()
+    
+    private let setButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Установить", for: .normal)
+        button.setTitleColor(UIColor(rgb: 0xFFFFFF), for: .normal)
+        button.backgroundColor = UIColor(rgb: 0xF26E11)
+        button.titleLabel?.font = UIFont.setAppMainFont(16)
+        button.layer.cornerRadius = 8
+        button.clipsToBounds = true
+        
+        //        self.addTarget(self, action: #selector(nil), for: .touchUpInside)
+        
+        return button
+    }()
     //MARK: - loading
     
     override func viewDidLoad() {
@@ -41,43 +128,51 @@ class SettingsViewController: UIViewController {
 extension SettingsViewController {
     private func setupViews() {
         view.backgroundColor = UIColor(rgb: 0x204EC7)
-        view.addSubview(tableView)
+        view.addSubview(windowView)
         
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.backgroundColor = UIColor(rgb: 0xE9EEFA)
-        tableView.layer.cornerRadius = 8
-        tableView.separatorStyle = .none
-        
-        tableView.register(SettingsTableViewCell.self, forCellReuseIdentifier: settingsCellID)
-//        tableView.register(SettingsHeaderView.self, forCellReuseIdentifier: settingsHeaderID)
-//        tableView.register(SettingsFooterView.self, forCellReuseIdentifier: settingsFooterID)
-        tableView.dataSource = self
-        tableView.delegate = self
+        windowView.addSubview(titleLabel)
+        windowView.addSubview(temperatureLabel)
+        windowView.addSubview(windSpeedLabel)
+        windowView.addSubview(dateFormateLabel)
+        windowView.addSubview(notificationsLabel)
+        windowView.addSubview(temperatureToggle)
+        windowView.addSubview(windSpeedToggle)
+        windowView.addSubview(dateFormateToggle)
+        windowView.addSubview(notificationsToggle)
+        windowView.addSubview(setButton)
+
         
         NSLayoutConstraint.activate([
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            tableView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            tableView.heightAnchor.constraint(equalToConstant: 330)
+            windowView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            windowView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            windowView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            
+            titleLabel.leadingAnchor.constraint(equalTo: windowView.leadingAnchor, constant: 20),
+            titleLabel.topAnchor.constraint(equalTo: windowView.topAnchor, constant: 27),
+            
+            temperatureLabel.leadingAnchor.constraint(equalTo: windowView.leadingAnchor, constant: 20),
+            temperatureLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            windSpeedLabel.leadingAnchor.constraint(equalTo: windowView.leadingAnchor, constant: 20),
+            windSpeedLabel.topAnchor.constraint(equalTo: temperatureLabel.bottomAnchor, constant: 30),
+            dateFormateLabel.leadingAnchor.constraint(equalTo: windowView.leadingAnchor, constant: 20),
+            dateFormateLabel.topAnchor.constraint(equalTo: windSpeedLabel.bottomAnchor, constant: 30),
+            notificationsLabel.leadingAnchor.constraint(equalTo: windowView.leadingAnchor, constant: 20),
+            notificationsLabel.topAnchor.constraint(equalTo: dateFormateLabel.bottomAnchor, constant: 30),
+            
+            setButton.leadingAnchor.constraint(equalTo: windowView.leadingAnchor, constant: 35),
+            setButton.topAnchor.constraint(equalTo: notificationsLabel.bottomAnchor, constant: 42),
+            setButton.trailingAnchor.constraint(equalTo: windowView.trailingAnchor, constant: -35),
+            setButton.bottomAnchor.constraint(equalTo: windowView.bottomAnchor, constant: -16),
+            setButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            temperatureToggle.topAnchor.constraint(equalTo: windowView.topAnchor, constant: 57),
+            temperatureToggle.trailingAnchor.constraint(equalTo: windowView.trailingAnchor, constant: -30),
+            windSpeedToggle.topAnchor.constraint(equalTo: temperatureToggle.bottomAnchor, constant: 20),
+            windSpeedToggle.trailingAnchor.constraint(equalTo: windowView.trailingAnchor, constant: -30),
+            dateFormateToggle.topAnchor.constraint(equalTo: windSpeedToggle.bottomAnchor, constant: 20),
+            dateFormateToggle.trailingAnchor.constraint(equalTo: windowView.trailingAnchor, constant: -30),
+            notificationsToggle.topAnchor.constraint(equalTo: dateFormateToggle.bottomAnchor, constant: 20),
+            notificationsToggle.trailingAnchor.constraint(equalTo: windowView.trailingAnchor, constant: -30)
         ])
-    }
-}
-//MARK: - UITableViewDataSource
-
-extension SettingsViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: settingsCellID, for: indexPath) as! SettingsTableViewCell
-        return cell
-    }
-}
-//MARK: - UITableViewDelegate
-
-extension SettingsViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 330
     }
 }
